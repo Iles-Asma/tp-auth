cd005d65e9843fb2424a56fd01d38b99f60160e2
Iles-Asma <iles.a@icloud.com>
2026-05-19 12:43:34 +0200

    Implement basic authentication for admin access
    Added authentication middleware to protect admin page.

--- server.js
+++ server.js
@@ -26,3 +26,26 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+const checkAuth = async (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+        res.setheader('WWW-Authenticate', 'Basic realm="Administration"');
+        return res.status(401).send('Authentication requise');
+    }
+    const base64 = authHeader.split(' ')[1];
+    const [username, password] = Buffer.from(base64, 'base64').toString().split(':');
+
+    const user = db.prepare('SELECT * FROM users WHERE username = ?').get(username);
+    if (user && (await bcrypt.compare(password, user.password_hash))) {
+        req.user = user;
+        next();
+    }else {
+        return res.status(401).send('identifiants invalides');
+    }
+}
+
+app.get('/admin-page', checkAuth, (req, res) => {
+    res.sendFile(__dirname + '/public/admin-page.html');
+});
